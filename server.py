47cece67a54de9f0d2aec71eb0e7b2101b5f49de
DaniilAnisimov <daniilanisimov2005@yandex.com>
2021-03-27 01:19:43 +0400

    Список профессий

--- server.py
+++ server.py
@@ -2,18 +2,24 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-port = 8090
+port = 8080
 host = "127.0.0.1"
 
 
+@app.route("/")
 @app.route("/<title>")
-def main(title):
-    return render_template("main.html", title=title)
+def main(title=""):
+    return render_template("base.html", title=title)
 
 
 @app.route("/training/<prof>")
 def training(prof):
     return render_template("training.html", prof=prof, title=prof)
+
+
+@app.route("/list_prof/<list>")
+def list_prof(list):
+    return render_template("list_prof.html", list=list)
 
 
 if __name__ == '__main__':
