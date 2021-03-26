2137999ea50c6e52afaa5cfd1197d43e0169abe3
DaniilAnisimov <daniilanisimov2005@yandex.com>
2021-03-27 02:36:18 +0400

    Автоматический ответ

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 port = 8080
 host = "127.0.0.1"
@@ -20,6 +19,18 @@ def training(prof):
 @app.route("/list_prof/<list>")
 def list_prof(list):
     return render_template("list_prof.html", list=list)
+
+
+@app.route("/answer")
+@app.route("/auto_answer")
+def answer():
+    questionnaire = {"title": "Watny Mark",
+                     "surname": "Watny", "name": "Mark",
+                     "education": "выше среднего", "profession": "штурман марсохода",
+                     "sex": "male",
+                     "motivation": "Всегда мечтал застрять на Марсе!",
+                     "ready": "True"}
+    return render_template("auto_answer.html", title=questionnaire["title"], questionnaire=questionnaire)
 
 
 if __name__ == '__main__':
