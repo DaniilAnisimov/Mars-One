e043dd9e91a1d6aa855768bee56c84495f61e4b6
DaniilAnisimov <daniilanisimov2005@yandex.com>
2021-04-20 00:02:51 +0400

    Ресурс Пользователи v2

--- tests/test_api_v2_users.py
+++ tests/test_api_v2_users.py
@@ -1,19 +1,20 @@
 from pprint import pprint
 from requests import get, post, delete
 
+# Получим всех пользователей
 pprint(get("http://localhost:8080/api/v2/users").json())
-
+# Добавим нового пользователя
 pprint(post("http://localhost:8080/api/v2/users", json={'id': 2, 'surname': "a", 'name': "b", 'age': 30,
                                                         'position': "of", 'speciality': "ph", 'address': "ktm",
                                                         "password": "125876", 'email': "sd",
                                                         "city_from": "Арвидсъяур"}).json())
-
+# Узнаем информацию о пользователе которого нет в бд
 pprint(get("http://localhost:8080/api/v2/users/23").json())
-
+# Узнаем информацию о пользователе
 pprint(get("http://localhost:8080/api/v2/users/2").json())
-
+# Удалим пользователя которого нет в бд: User 25 not found
 pprint(delete("http://localhost:8080/api/v2/users/25").json())
-
+# Удалим существующего пользователя
 pprint(delete("http://localhost:8080/api/v2/users/2").json())
-
+# Получим всех пользователей
 pprint(get("http://localhost:8080/api/v2/users").json())
